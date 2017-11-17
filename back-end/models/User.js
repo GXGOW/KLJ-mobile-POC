02856f05e17b72bd16458bc3c160855197b11d4b
Nicolas Loots <nicolas.loots@outlook.be>
2017-11-17 10:41:08 +0100

    Add admin role

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -3,50 +3,50 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        validate:  {
-            validator: function(v){
-                return '/^[a-z0-9_\-\.]{2,}@[a-z0-9_\-\.]{2,}\.[a-z]{2,}$/i'.test(v);
-            },
-            message: 'Ongeldig e-mailadres'
-        }
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['leider','lid']
-    },
-    address: {
-        street: {
-            type:String
-        },
-        number: {
-            type: String
-        },
-        postalCode: {
-            type: Number
-        },
-        City: {
-            type: String
-        }
-    },
-    phoneNumber: {
-        type: String,
-        validate: {
-            vaildator: function(v) {
-                return '/[0-9]{10}/'.test(v);
-            }
-        }
-    },
-    birthday: {
-        type: Date,
-        required: true
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return '/^[a-z0-9_\-\.]{2,}@[a-z0-9_\-\.]{2,}\.[a-z]{2,}$/i'.test(v);
+      },
+      message: 'Ongeldig e-mailadres'
     }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['leider', 'lid', 'admin']
+  },
+  address: {
+    street: {
+      type: String
+    },
+    number: {
+      type: String
+    },
+    postalCode: {
+      type: Number
+    },
+    City: {
+      type: String
+    }
+  },
+  phoneNumber: {
+    type: String,
+    validate: {
+      vaildator: function (v) {
+        return '/[0-9]{10}/'.test(v);
+      }
+    }
+  },
+  birthday: {
+    type: Date,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
