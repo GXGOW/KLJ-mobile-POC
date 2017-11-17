f3534adfd3c9e721e26c9bfad8fb4a6567054ba6
Nicolas Loots <nicolas.loots@outlook.be>
2017-11-17 11:31:29 +0100

    Add generate JWT method

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -2,6 +2,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var datejs = require('date.js');
+
 
 var UserSchema = new Schema({
   email: {
@@ -50,6 +53,16 @@ UserSchema.methods.setPassword = function (password) {
 UserSchema.methods.validPassword = function (password) {
   let hash = crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('hex');
   return this.hash === hash;
+}
+
+UserSchema.methods.generateJWT = function () {
+  var today = new Date();
+  var exp = datejs('in 2 weeks');
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000)
+  }, process.env.NUCLEAR_LAUNCH_CODES);
 }
 
 module.exports = mongoose.model('User', UserSchema);
