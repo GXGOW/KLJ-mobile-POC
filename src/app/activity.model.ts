93128209a0799816defae62e5fa956f90b38781d
Nicolas Loots <nicolas.loots@outlook.be>
2017-11-23 22:46:39 +0100

    Change property names to the same names as the Activity Schema

--- src/app/activity.model.ts
+++ src/app/activity.model.ts
@@ -1,22 +1,22 @@
 export class Activity {
-    private _name: string;
+    private _title: string;
     private _description: string;
     private _organisedBy: [string];
     private _date: Date;
     private _location: string;
     private _attendees: [string];
 
-    constructor(name: string, description: string) {
-        this._name = name;
+    constructor(title: string, description: string) {
+        this._title = title;
         this._description = description;
     }
 
-    get name(): string {
-        return this._name;
+    get title(): string {
+        return this._title;
     }
 
-    set name(name: string) {
-        this._name = name;
+    set title(title: string) {
+        this._title = title;
     }
 
     get description(): string {
