3c73ff3b833dfc3cf12b2bdcbade09ebda2ea7ba
Nicolas Loots <nicolas.loots@outlook.be>
2017-11-24 11:25:38 +0100

    Add add-activity popup dialog functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { ActivityComponent } from './activity/activity.component';
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { AddActivityComponent } from './add-activity/add-activity.component';
+import { FormsModule } from '@angular/forms';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     ActivityComponent,
-    ActivityListComponent
+    ActivityListComponent,
+    AddActivityComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents: [
+    AddActivityComponent
   ],
   bootstrap: [AppComponent]
 })
