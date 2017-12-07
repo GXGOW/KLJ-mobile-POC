921d3bc9b6b0bd1b522647f643bef40f6606db81
Nicolas Loots <nicolas.loots@outlook.be>
2017-12-07 12:01:59 +0100

    - Disable attend-button when request is busy - Change color based on request response

--- src/app/act/activity-detail/activity-detail.component.ts
+++ src/app/act/activity-detail/activity-detail.component.ts
@@ -3,7 +3,8 @@ import { Activity } from '../activity.model';
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { AuthenticationService } from '../../user/authentication.service';
-
+declare const jquery: any;
+declare const $: any;
 @Component({
   selector: 'app-activity-detail',
   templateUrl: './activity-detail.component.html',
@@ -18,7 +19,11 @@ export class ActivityDetailComponent implements OnInit {
   }
 
   joinActivity() {
-    this._activityDataService.joinActivity(this.activity.id).subscribe(item => console.log(item));
+    $('#attendBtn').addClass('disabled');
+    this._activityDataService.joinActivity(this.activity.id).subscribe(item => {
+      $('#attendBtn').removeClass('disabled');
+      item ? $('#attendBtn').removeClass('red').addClass('green') : $('#attendBtn').removeClass('green').addClass('red');
+    });
   }
 
   close() {
