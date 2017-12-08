4913ed322c512a59d2d53754ed90196af97931bf
Nicolas Loots <nicolas.loots@outlook.be>
2017-12-08 01:27:47 +0100

    - Disable button when not all fields are filled in - Add error message when username/password is wrong

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,7 +2,7 @@ import { AuthenticationService } from '../authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
+declare const $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -19,18 +19,21 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.user = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
   onSubmit() {
     this.authService.login(this.user.value.username, this.user.value.password).subscribe(val => {
       if (val) {
+        $('.errMsg').hide();
         this.dialogRef.close();
         location.reload();
+      } else {
+        $('.errMsg').show();
       }
-    }, err => this.error = err.json().message);
+    }, err => $('.errMsg').show());
   }
 
   close() {
